b79c85e7817c139d1ab74772bfb65f05a086d85d
subs <1262945983@qq.com>
2021-01-18 23:54:51 +0800

    时间控件

--- src/components/PunchCard.vue
+++ src/components/PunchCard.vue
@@ -1,9 +1,18 @@
 <template>
     <div id="PunchCard">
-        <el-select v-model="value" placeholder="请选择" @click.native='getOption'>
+        <!-- <el-select v-model="value" placeholder="请选择" @click.native='getOption'>
             <el-option v-for="item in option" :key="item.value" :label="item.label" :value="item.value">
             </el-option>
-        </el-select>
+        </el-select> -->
+        <button @click ='plus'>+</button>
+        {{ input }}
+        <button @clicik ='sub'>-</button>
+        <br>
+
+        <input v-model="date" @click='getDate' placeholder="日期" value="date">
+
+        <el-calendar v-model="date" v-if='flag'></el-calendar>
+
     </div>
 </template>
 
@@ -12,7 +21,10 @@
         name: 'PunchCard',
         data() {
             return {
-                option: [], value: ''
+                option: [], value: '',
+                input: '1',
+                flag:false,
+                date:new Date()
             }
         },
         methods: {
@@ -28,6 +40,23 @@
                 for (let index = 1; index < 56; index++) {
                     this.option.push({value:index,label:index});
                 }
+            },
+            plus(){
+                var _this = this;
+              _this.input++;
+        
+            },
+            sub(){
+                var _this = this;
+                if(_this.input>=2){
+                    _this.input--;
+                }else{
+                    _this.input =1;
+                }
+            },
+            getDate(){
+                var _this = this;
+                _this.flag = !_this.flag;
             }
         },
         components: {},
